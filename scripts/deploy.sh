eae9f8a252ceb491477716b6c0cd8e04b886d9bb
Ryuichi Komeda <komeda@hivelocity.co.jp>
2016-06-13 21:46:48 +0900

    Modifying bower install process on github pages deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,8 +10,6 @@ ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github
 echo "Compiling new static content"
 mkdir $TEMP_DIRECTORY || exit 1
 harp compile jade $TEMP_DIRECTORY/demo || exit 1
-bower install https://github.com/hivelocityinc/adminize.git || exit 1
-cp -r ./bower_components $TEMP_DIRECTORY || exit 1
 
 echo "Checking out gh-pages branch"
 git checkout -B gh-pages || exit 1
@@ -21,7 +19,9 @@ git rm -rf . || exit 1
 
 echo "Copying newly generated static content"
 cp -r $TEMP_DIRECTORY/* . || exit 1
-cp -r $TEMP_DIRECTORY/bower_components . || exit 1
+bower install https://github.com/hivelocityinc/adminize.git || exit 1
+touch .gitignore || exit 1
+echo "node_modules" >> .gitignore || exit 1
 
 echo "Pushing new content to $ORIGIN_URL"
 git config user.name "Travis-CI" || exit 1
